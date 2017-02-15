f231e4e125f5a73bd78ce4a5a993b7440627fd25
chaandchopra <chaandchopra@gmail.com>
2017-02-15 18:01:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,26 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('ui/css/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'js','bootstrap.min.css'));
+});
+
+app.get('ui/js/bootstrap.min.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'js','bootstrap.min.js'));
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
